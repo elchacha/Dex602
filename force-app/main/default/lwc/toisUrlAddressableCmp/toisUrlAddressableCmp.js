c7a4755d5384b3c3f3a057470bb9b98f991b9089
elchacha <fabricechallier@gmail.com>
2026-02-05 14:11:36 +0100

    Enhance ToisUrlAddressableCmp with api decorator
    Added api decorator to the component and updated comments for clarity.

--- force-app/main/default/lwc/toisUrlAddressableCmp/toisUrlAddressableCmp.js
+++ force-app/main/default/lwc/toisUrlAddressableCmp/toisUrlAddressableCmp.js
@@ -1,14 +1,24 @@
-import { LightningElement ,wire} from 'lwc';
+import { LightningElement ,wire,api} from 'lwc';
 import {CurrentPageReference} from 'lightning/navigation';
 
 export default class ToisUrlAddressableCmp extends LightningElement {
 
     // to open the url , use the CAMELCASE name of the lwc. DO NOT TRUST THE OFFICIAL DOCUMENTATION
+    //   url :  /lightning/cmp/c__toisUrlAddressableCmp?c__simpleValue=value&c__simpleValue2=anotherValue
+
 
     @wire(CurrentPageReference)
-    currentPageRef;
-    
-    get urlName(){
-        return this.currentPageRef.state.c__propertyValue;
-    }
+    currentPageRef
+
+    /*
+        This is the way to use the CurrentPageReference to get URL parameters values simply in html template
+        valuesFromUrl;
+
+        @wire(CurrentPageReference)
+        currentPageRef(result) {
+            this.valuesFromUrl = result.state;  
+        }
+    */
+
+
 }
